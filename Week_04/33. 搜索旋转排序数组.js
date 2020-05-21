b05d1d2eb704d2a553685951a90d7240c3ccb722
lijiaolong <ljl168241296>
2020-05-21 16:53:19 +0800

    排序数组

--- Week_04/33. 搜索旋转排序数组.js
+++ Week_04/33. 搜索旋转排序数组.js
@@ -23,17 +23,12 @@ let search = function(nums,target){
     while(left <= right){
         let mid = ~~(left+(right-left)/2);
         if(nums[mid] === target) return mid;
-        if(nums[mid] >= nums[left])
+        (nums[mid] >= nums[left]) ?
             // 在左边是有序的
-            if(target >= nums[left] && target <= nums[mid])
-                right = mid - 1;
-            else
-                left = mid + 1;    
-        else
-            if(target >= nums[mid] && target <= nums[right])
-                left = mid + 1;
-            else
-                right = mid - 1;
+            (target >= nums[left] && target <= nums[mid]) ? right = mid - 1 : left = mid + 1
+            : 
+            (target >= nums[mid] && target <= nums[right]) ?  left = mid + 1 : right = mid - 1;
+                
     }
     return -1;
 }
